898f5ae494e2251b9f111b12ddb7451583359a77
Sebastian Maludziński <87277813+SMaludiznski@users.noreply.github.com>
2022-03-11 15:52:40 +0100

    Add files via upload
    - color extension

---
+++ CryptoExchangeApp/Base/Extensions/UIColor+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+extension.swift
+//  CryptoExchangeApp
+//
+//  Created by Sebastian Maludziński on 08/03/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let backgroundColor = UIColor(named: "Background")
+    static let cellBackgroundColor = UIColor(named: "CellBackground")
+    static let imageBackgroundColor = UIColor(named: "ImageBackground")
+    static let fontColor = UIColor(named: "Font")
+    static let appRedColor = UIColor(named: "AppRed")
+    static let appGreenColor = UIColor(named: "AppGreen")
+}
